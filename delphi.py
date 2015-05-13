e1652394daf073ac2bdb48077550a5a020e88c99
Clayton <clayton.aa@gmail.com>
2015-05-13 14:20:39 -0400

    Finalização das adaptações no script delphi.py

---
+++ delphi.py
@@ -0,0 +1,24 @@
+#coding: utf8
+import os
+import subprocess
+
+FNULL = open(os.devnull, 'wb')
+
+def resource_compile(resource_filename, output_filename):
+    params = "brcc32 %s -fo%s" % (resource_filename, output_filename)
+    subprocess.call(params.split(), stdout=FNULL)
+
+def build_project(project_filename):
+    print u"Compilando projeto %s..." % project_filename,
+    params = "dcc32 -$O+ -$W+ -$D- -$L- -$Y- -$C- -q -b".split()
+    params.append(project_filename)
+
+    dcc32 = subprocess.Popen(params, stdout=FNULL)
+    exit_code = dcc32.wait()
+
+    if exit_code!=0:
+        print
+        print u'-> Erro na compilação do projeto %s' % project_filename
+    else:
+        print u"Compilado com sucesso!"
+
