69017304e3c9f17244e84b6bc24fba7d1f1427e4
Clayton <clayton.aa@gmail.com>
2015-05-13 14:22:11 -0400

    Adicionado arquivo utils.py com funções utilitárias

---
+++ utils.py
@@ -0,0 +1,31 @@
+import os
+import contextlib
+import functools
+
+def memoize(obj):
+    """ Decorator que implementa um memoize
+    """
+    cache = obj.cache = {}
+
+    @functools.wraps(obj)
+    def memoizer(*args, **kwargs):
+        if args not in cache:
+            cache[args] = obj(*args, **kwargs)
+        return cache[args]
+    return memoizer
+
+@contextlib.contextmanager
+def temp_chdir(path):
+    """
+    Usage:
+    >>> with temp_chdir(gitrepo_path):
+    ...   subprocess.call('git status')
+    """
+    starting_directory = os.getcwd()
+    try:
+        os.chdir(path)
+        yield
+    finally:
+        os.chdir(starting_directory)
+
+
