301231d8e36775a123e97c8db3163dc2ebbae106
Clayton <clayton.aa@gmail.com>
2017-07-25 13:40:40 -0400

    Aprimoramento no gerenciador de dependências

--- configure.py
+++ configure.py
@@ -5,46 +5,26 @@ import subprocess
 import argparse
 
 PASTA_COMPONENTES = os.getenv('COMPONENTES')
+PASTA_PROJETO_ELLO = PASTA_COMPONENTES + "\\.." 
+FNULL = open(os.devnull, 'w')
 
-def get_revision_from_file(arquivo):
-    arquivo.readline()
-    return arquivo.readline().split(':')[1].strip()
-
-def checkout_revision(package_name, revision_hash):
-    print u'=> Atualizando {0} ({1})'.format(package_name, revision_hash[0:7])
-    os.chdir("{0}/{1}".format(PASTA_COMPONENTES, package_name))
-    FNULL = open(os.devnull, 'w')
-    command = 'git checkout {0}'.format(revision_hash).split()
-    return_code = subprocess.call(command, stdout=FNULL, stderr=subprocess.STDOUT)
-
-    # Se der erro ao fazer o checkout da revisão, tenta fazer um fetch e fazer o checkout novamente
-    if return_code!=0:
-        fetch_reporitory('excellent')
-        subprocess.call(command, stdout=FNULL, stderr=subprocess.STDOUT)
-
-def fetch_reporitory(package_name):
-    print "Fazendo fetch do repositorio", package_name
-    FNULL = open(os.devnull, 'w')
-    os.chdir(PASTA_COMPONENTES + '/' + package_name)
-    subprocess.call('git fetch', stdout=FNULL, stderr=subprocess.STDOUT)
-
-def update_requirements():
+def atualiza_dependencias():
     with open('LEIAME.txt', 'r') as arquivo_leiame:
         while True:
             linha = arquivo_leiame.readline()
-
             if not linha:
                 break
-
             if re.match('^Excellent', linha):
-                revisao = get_revision_from_file(arquivo_leiame)
-                checkout_revision('excellent', revisao)
-
+                codigo_hash = extrai_hash_do_pacote(arquivo_leiame)
+                checkout_pacote('excellent', PASTA_COMPONENTES, codigo_hash)
             if re.match('^ACBr', linha):
-                revisao = get_revision_from_file(arquivo_leiame)
-                checkout_revision('trunk2', revisao)
+                codigo_hash = extrai_hash_do_pacote(arquivo_leiame)
+                checkout_pacote('trunk2', PASTA_COMPONENTES, codigo_hash)
+            if re.match('^Ello', linha):
+                codigo_hash = extrai_hash_do_pacote(arquivo_leiame)
+                checkout_pacote('ello', PASTA_PROJETO_ELLO, codigo_hash)
 
-def freeze_requirements():
+def congela_dependencias():
     saida = u"""
 Excellent
   Repositório : git@bitbucket.org:ellotecnologia/excellent.git
@@ -54,28 +34,54 @@ ACBr
   Repositório : ssh://git@10.1.1.100:2202/var/repos/acbr-trunk2.git
   Revisão     : {1}
 """
-    revisao_excellent  = get_last_commit('{0}/Excellent'.format(PASTA_COMPONENTES))
-    revisao_acbr       = get_last_commit('{0}/trunk2'.format(PASTA_COMPONENTES))
+    revisao_excellent  = obtem_ultimo_commit('{0}/Excellent'.format(PASTA_COMPONENTES))
+    revisao_acbr       = obtem_ultimo_commit('{0}/trunk2'.format(PASTA_COMPONENTES))
     print saida.format(
         revisao_excellent,
         revisao_acbr
     )
 
-def get_last_commit(path):
-    os.chdir(path)
-    FNULL = open(os.devnull, 'w')
+def extrai_hash_do_pacote(arquivo):
+    arquivo.readline()
+    return arquivo.readline().split(':')[1].strip()
+
+def checkout_pacote(nome_pacote, caminho, hash_revisao):
+    print u'=> Atualizando {0} ({1})'.format(nome_pacote, hash_revisao[0:7])
+    os.chdir("{0}/{1}".format(caminho, nome_pacote))
+    if not (copia_de_trabalho_limpa()):
+        print u"Existem modificações não commitadas na pasta {0}\\{1}. Cancelando atualização.".format(caminho, nome_pacote)
+        return
+    command = 'git checkout {0}'.format(hash_revisao).split()
+    return_code = subprocess.call(command, stdout=FNULL, stderr=subprocess.STDOUT)
+
+    # Se der erro ao fazer o checkout da revisão, tenta fazer um fetch e fazer o checkout novamente
+    if return_code!=0:
+        baixa_atualizacoes_do_repositorio(nome_pacote)
+        subprocess.call(command, stdout=FNULL, stderr=subprocess.STDOUT)
+
+def copia_de_trabalho_limpa():
+    return_code = subprocess.call("git diff-index --quiet HEAD --", stdout=FNULL, stderr=subprocess.STDOUT)
+    return return_code==0
+
+def baixa_atualizacoes_do_repositorio(nome_pacote):
+    print "Fazendo fetch do repositorio", nome_pacote
+    os.chdir(PASTA_COMPONENTES + '/' + nome_pacote)
+    subprocess.call('git fetch', stdout=FNULL, stderr=subprocess.STDOUT)
+
+def obtem_ultimo_commit(caminho):
+    os.chdir(caminho)
     command = 'git log -1 --pretty=format:%H'
     git = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
     out, err = git.communicate()
     return out
 
 if __name__=="__main__":
-    parser = argparse.ArgumentParser(description='Ello Requirements management tool')
+    parser = argparse.ArgumentParser(description='Ferramenta de gerenciamento de dependencias de pacotes Ello')
     parser.add_argument('--freeze', action="store_true", default=False)
     args = parser.parse_args()
 
     if args.freeze:
-        freeze_requirements()
+        congela_dependencias()
     else:
-        update_requirements()
+        atualiza_dependencias()
 
