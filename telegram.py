03ce4ae7bb1602641c1c912eda651b0d22e938c7
Clayton <clayton.aa@gmail.com>
2017-08-04 09:36:38 -0400

    Adicionei envio de mensagem com markdown para o telegram

--- telegram.py
+++ telegram.py
@@ -1,23 +1,25 @@
 #coding: utf8
 import os
-import logging
-import telepot
 
+import telepot
+import logging
 import config
 
 logger = logging.getLogger()
 
+# -211240257
+# @ellotecnologia
 def envia_mensagem(mensagem):
     bot = telepot.Bot(config.telegram_token)
-    bot.sendMessage('@ellotecnologia', mensagem)
+    bot.sendMessage('@ellotecnologia', mensagem, parse_mode='Markdown')
 
 def notifica():
     logger.info(u"Notificando suporte via Telegram...")
-    mensagem = u"Nova revisão disponível para download\n\n"
+    mensagem = u"(*Ello*) Nova revisão disponível\n\n"
     # temp.txt é o arquivo criado ao gerar o changelog
     with open('temp.txt') as f:
         mensagem += f.read().decode('latin1')
-    mensagem += "\n\nhttp://wiki.ellotecnologia.net.br/wiki:changelog"
+    mensagem += "\n\n[Changelog](http://wiki.ellotecnologia.net.br/wiki:changelog)"
     os.remove('temp.txt')
     envia_mensagem(mensagem)
 
