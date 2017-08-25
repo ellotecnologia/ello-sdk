38b3c699e9432e4c0efab8ca1b12ccfdce7c866b
Clayton <clayton.aa@gmail.com>
2017-08-25 17:24:05 -0400

    Improved build pipeline to handle multiple projects

--- pipeline/deploy_pipeline.py
+++ pipeline/deploy_pipeline.py
@@ -9,20 +9,38 @@ from pipeline import setup_builder
 from pipeline import wiki
 from pipeline import signtool
 from pipeline import changelog
+from pipeline import git
 from pipeline.packer import empacota, gera_sfx
 from pipeline.notifications import notify_team
 from pipeline.test_pipeline import run_test_pipeline
 
 def build_and_deploy(project, args):
-    #if args.run_tests:
-    #    run_test_pipeline()
+    # quick fix (kludge) to handle different projects
+    if project.name.lower()=='ello':
+        build_and_deploy_ello(project, args)
+    else:
+        build_and_deploy_other_project(project, args)
 
+def build_and_deploy_other_project(project, args):
     delphi.build_project(project, args)
+    artifact_name = project.output_folder + "\\{0}.exe".format(project)
+    #shutil.copyfile('Ello.cfg.debug', 'Ello.cfg')
+    deployer.deploy(artifact_name)
+    if args.update_wiki:
+        wiki.update_wiki_pages(project)
+    if args.notify_deploy_action:
+        notify_team(project)
+
+def build_and_deploy_ello(project, args):
+    if args.run_tests:
+        run_test_pipeline()
+
+    project.version = git.get_latest_tag()
+    delphi.build_project(project, debug=False)
 
     artifact_name = project.output_folder + "\\{0}.exe".format(project)
-    #signtool.sign(artifact_name)
-    #artifact_name = empacota_executavel(artifact_name)
-    #shutil.copyfile('Ello.cfg.debug', 'Ello.cfg')
+    signtool.sign(artifact_name)
+    artifact_name = build_packed_artifact(project, artifact_name)
     deployer.deploy(artifact_name)
 
     #if args.build_installer:
@@ -32,12 +50,11 @@ def build_and_deploy(project, args):
     if args.notify_deploy_action:
         notify_team(project)
 
-def empacota_executavel(nome_executavel):
-    versao = '.'.join(changelog.ultima_versao())
-    nome_pacote = "{0}\\Ello-{1}".format(output_folder(), versao)
-    nome_pacote = empacota(nome_executavel, nome_pacote)
-    nome_pacote = gera_sfx(nome_pacote)
-    return nome_pacote
+def build_packed_artifact(project, executable_name):
+    package_name = "{0}\\{1}-{2}".format(project.output_folder, project.name, project.version)
+    package_name = empacota(executable_name, package_name)
+    package_name = gera_sfx(package_name)
+    return package_name
 
 #def build_and_deploy_pre_release():
 #    nome_executavel = output_folder() + "\\Ello.exe"
