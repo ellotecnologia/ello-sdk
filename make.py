46b6a92f3433695e7c3c4f767775a4b18a197da5
Clayton <clayton.aa@gmail.com>
2017-10-09 13:38:34 -0400

    Padronização do código

--- make.py
+++ make.py
@@ -1,5 +1,4 @@
-#coding: utf8
-import sys
+# encoding: utf8
 import logging
 import argparse
 import json
@@ -19,8 +18,10 @@ logging.basicConfig(format='=> %(message)s')
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
+
 class PackageFileNotFound(Exception):
     pass
+
 
 def get_project_name():
     package_file = 'package.json'
@@ -29,6 +30,7 @@ def get_project_name():
     with open(package_file, 'r') as f:
         package = json.load(f)
     return package['name']
+
 
 def parse_args():
     parser = argparse.ArgumentParser(description=u"Utilitário para build e deploy do projeto ello")
@@ -43,42 +45,57 @@ def parse_args():
     resource_cmd  = commands.add_parser("resource", help=u"Atualiza arquivo de resources")
     pre_cmd       = commands.add_parser("pre", help=u"Cria um pre-release e envia para o servidor")
 
-    all_cmd.add_argument("--debug", type=bool, default=True, help=u"Compila com modo debug ativo")
+    all_cmd.add_argument("--debug", type=bool, default=True, 
+                         help=u"Compila com modo debug ativo")
 
-    deploy_cmd.add_argument("--debug", type=bool, default=False, help=u"Compila com modo debug ativo")
-    deploy_cmd.add_argument("--ignore-installer", default=True, action="store_false", dest="build_installer", help=u"Não faz o build do instalador")
-    deploy_cmd.add_argument("--ignore-wiki", default=True, action="store_false", dest="update_wiki", help=u"Atualizar wiki no servidor")
-    deploy_cmd.add_argument("--ignore-tests", default=True, action="store_false", dest="run_tests", help=u"Ignora a execução da suite de testes")
-    deploy_cmd.add_argument("--no-notification", default=True, action="store_false", dest="notify_deploy_action", help=u"Notificar sobre o deploy")
+    deploy_cmd.add_argument("--debug", type=bool, default=False,
+                            help=u"Compila com modo debug ativo")
+    deploy_cmd.add_argument("--ignore-installer", default=True,
+                            action="store_false",
+                            dest="build_installer",
+                            help=u"Não faz o build do instalador")
+    deploy_cmd.add_argument("--ignore-wiki", default=True,
+                            action="store_false",
+                            dest="update_wiki",
+                            help=u"Atualizar wiki no servidor")
+    deploy_cmd.add_argument("--ignore-tests", default=True,
+                            action="store_false",
+                            dest="run_tests",
+                            help=u"Ignora a execução da suite de testes")
+    deploy_cmd.add_argument("--no-notification", default=True,
+                            action="store_false",
+                            dest="notify_deploy_action",
+                            help=u"Notificar sobre o deploy")
 
     return parser.parse_args()
+
 
 def main():
     args = parse_args()
     command = args.command
     project = delphi.DelphiProject(get_project_name())
 
-    if command=='all':
+    if command == 'all':
         delphi.build_project(project, args)
-    elif command=='deploy':
+    elif command == 'deploy':
         deploy_pipeline.build_and_deploy(project, args)
-    elif command=='changelog':
+    elif command == 'changelog':
         changelog.update_changelog(project)
-    elif command=='clean':
+    elif command == 'clean':
         clean_working_dir()
-    elif command=='resource':
+    elif command == 'resource':
         last_tag = git.get_latest_tag().split('.')
         delphi.rc_file.update_resource_file(project, *last_tag)
-    elif command=='wiki':
+    elif command == 'wiki':
         wiki.update_wiki_pages(project)
-    elif command=='notify':
+    elif command == 'notify':
         notify_team(project)
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     try:
         main()
     except delphi.BuildError:
         logger.info(u'Erro na compilação do projeto!')
     except test_pipeline.IntegrationTestError, e:
         logger.info(e.message)
-
--- pipeline/notifications/telegram.py
+++ pipeline/notifications/telegram.py
@@ -1,5 +1,4 @@
 #coding: utf8
-import os
 import logging
 
 import telepot
