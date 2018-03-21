5884adbc50c36470b377de46e3c91b67ba4458bc
Clayton <clayton.aa@gmail.com>
2018-03-21 10:14:03 -0400

    Corrige gravação de arquivos ini com CRLF ao invés de somente LF

--- delphi/custom_config_parser.py
+++ delphi/custom_config_parser.py
@@ -11,16 +11,16 @@ class CustomConfigParser(ConfigParser.ConfigParser):
             and Sections doesn't end with a newline
         """
         if self._defaults:
-            fp.write("[%s]\n" % DEFAULTSECT)
+            fp.write("[%s]\r\n" % DEFAULTSECT)
             for (key, value) in self._defaults.items():
-                fp.write("%s = %s\n" % (key, str(value).replace('\n', '\n\t')))
-            fp.write("\n")
+                fp.write("%s = %s\r\n" % (key, str(value).replace('\n', '\r\n\t')))
+            fp.write("\r\n")
         for section in self._sections:
-            fp.write("[%s]\n" % section)
+            fp.write("[%s]\r\n" % section)
             for (key, value) in self._sections[section].items():
                 if key == "__name__":
                     continue
                 if (value is not None) or (self._optcre == self.OPTCRE):
                     # This is the important departure from ConfigParser for what you are looking for
-                    key = "=".join((key, str(value).replace('\n', '\n\t')))
-                fp.write("%s\n" % (key))
+                    key = "=".join((key, str(value).replace('\n', '\r\n\t')))
+                fp.write("%s\r\n" % (key))
