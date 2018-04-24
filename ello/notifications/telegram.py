95b5e72dbea39afce1b2fb2890a10f8b2bf96900
Clayton <clayton.aa@gmail.com>
2018-04-24 10:04:28 -0400

    Correção no envio de notificações via telegram

--- ello/notifications/telegram.py
+++ ello/notifications/telegram.py
@@ -1,28 +1,39 @@
-#coding: utf8
+# encoding: utf8
+from __future__ import unicode_literals
+
 import os
 import logging
 
 import telepot
+
 from ello.sdk import config
 
 logger = logging.getLogger()
 
-TMP_CHANGELOG_FILE = os.environ.get('TEMP') + '\\ell_changelog.tmp'
+chat_id = '@ellotecnologia'
 
-# -211240257
-# @ellotecnologia
+
+def get_changelog_text():
+    changelog = os.environ.get('TEMP') + '\\ell_changelog.tmp'
+    with open(changelog) as f:
+        text = f.read().decode('latin1')
+    return text
+
+
 def send_message(mensagem):
+    mensagem = mensagem.replace("_", "\\_")  # Se não substituir dispara telepot.exception.TelegramError
     bot = telepot.Bot(config.telegram_token)
-    bot.sendMessage('@ellotecnologia', mensagem, parse_mode='Markdown')
+    bot.sendMessage(chat_id, mensagem, parse_mode='Markdown')
+
 
 def send_notification(project_name):
-    logger.info(u"Notificando time via Telegram...")
-    mensagem = u"(*{0}*) Nova revisão disponível\n\n".format(project_name)
-    with open(TMP_CHANGELOG_FILE) as f:
-        mensagem += f.read().decode('latin1')
+    logger.info("Notificando time via Telegram...")
+    mensagem = "(*{0}*) Nova revisão disponível\n\n".format(project_name)
+    mensagem += get_changelog_text()
     mensagem += "\n[Changelog](http://wiki.ellotecnologia.net.br/wiki:changelog:{0})".format(project_name)
-    send_message(mensagem)
+    send_message(mensagem)    
+
 
 if __name__=="__main__":
-    send_notification()
-
+    chat_id = '-211240257'  # Chat de testes
+    send_notification('Ello')
