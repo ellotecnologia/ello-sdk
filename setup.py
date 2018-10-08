deeef9c5198edc10373a6fabb6472f4fe9f80ce9
Clayton <clayton.aa@gmail.com>
2018-10-08 15:32:24 -0400

    Correção na versão do telepot

--- setup.py
+++ setup.py
@@ -14,12 +14,11 @@ setup(
         'fdb==1.8',
         'dokuwikixmlrpc',
         'requests',
-        'telepot==12.5',
         'colorama==0.3.9',
         'pyperclip',
         'sqlparse==0.2.4',
         'Pygments==2.2.0',
-        'telepot==12.5',
+        'telepot',
         'future==0.16.0',
         'configparser'
     ],
