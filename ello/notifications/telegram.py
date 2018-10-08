5f3942e63d424f07162778a6d1c04ab75a280b2e
Clayton <clayton.aa@gmail.com>
2018-10-08 15:33:47 -0400

    Aprimoramento no processo de envio de notificações via telegram
    Para utilizar unicode literals é necessário que o token e demais dados
    passados para o telepot sejam codificados antes.

--- ello/notifications/telegram.py
+++ ello/notifications/telegram.py
@@ -11,6 +11,15 @@ from ello.sdk import config
 logger = logging.getLogger()
 
 chat_id = '@ellotecnologia'
+WIKI_URL = 'http://wiki.ellotecnologia.net.br'
+
+
+def send_notification(project_name):
+    logger.info("Notificando time via Telegram...")
+    mensagem = "(*{0}*) Nova revisão disponível\n\n".format(project_name)
+    mensagem += get_changelog_text()
+    mensagem += "\n[Changelog]({0}/wiki:changelog:{1})".format(WIKI_URL, project_name)
+    send_message(mensagem)    
 
 
 def get_changelog_text():
@@ -22,18 +31,10 @@ def get_changelog_text():
 
 def send_message(mensagem):
     mensagem = mensagem.replace("_", "\\_")  # Se não substituir dispara telepot.exception.TelegramError
-    bot = telepot.Bot(config.telegram_token)
-    bot.sendMessage(chat_id, mensagem, parse_mode='Markdown')
-
-
-def send_notification(project_name):
-    logger.info("Notificando time via Telegram...")
-    mensagem = "(*{0}*) Nova revisão disponível\n\n".format(project_name)
-    mensagem += get_changelog_text()
-    mensagem += "\n[Changelog](http://wiki.ellotecnologia.net.br/wiki:changelog:{0})".format(project_name)
-    send_message(mensagem)    
+    bot = telepot.Bot(config.telegram_token.encode('utf8'))
+    bot.sendMessage(chat_id.encode('utf8'), mensagem.encode('utf8'), parse_mode='Markdown')
 
 
 if __name__=="__main__":
     chat_id = '-211240257'  # Chat de testes
-    send_notification('Ello')
+    send_message('Teste com acentuação!')
