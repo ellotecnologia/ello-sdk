8f759f999e0cd804f2c39a5f8eb5af60cc9d8ea9
Clayton <clayton.aa@gmail.com>
2020-03-30 14:15:06 -0400

    Adicionada busca em objetos TcxLabelEdit e objetos inherited

--- ello/cli/dfmgrep.py
+++ ello/cli/dfmgrep.py
@@ -4,14 +4,16 @@ import glob
 from enum import Enum
 from collections import defaultdict
 
-RE_QUERY = re.compile('object (\w+): TSQLQuery')
+RE_QUERY  = re.compile('(object|inherited) (\w+): TSQLQuery')
+RE_LBEDIT = re.compile('(object|inherited) (\w+): TcxLabelEdit( \[\d+\])*')
 
 class State(Enum):
     OUTER = 1
     QUERY = 2
     STRINGLIST = 3
     LINEMERGE = 4
-
+    PARAMS = 5
+    LABELEDIT = 6
 
 def clear_line(line):
     line = line.strip(')')
@@ -25,7 +27,7 @@ def clear_line(line):
 def parse_queries_from_dfm(filename):
     """Processa arquivos .dfm gerando um dict com o nome da query
        e suas respectivas linhas"""
-    query_name = ''
+    component_name = ''
     state = State.OUTER
     queries = defaultdict(list)
     with open(filename, 'r', encoding='latin1') as dfm:
@@ -36,25 +38,40 @@ def parse_queries_from_dfm(filename):
             if state == State.OUTER:
                 match = RE_QUERY.match(line)
                 if match:
-                    query_name = match.group(1)
+                    component_name = match.group(2)
                     state = State.QUERY
+                    continue
+                match = RE_LBEDIT.match(line)
+                if match:
+                    component_name = match.group(2)
+                    state = State.LABELEDIT
             elif state == State.QUERY:
                 if line == 'end':
                     state = State.OUTER
+                elif 'Params = <' in line:
+                    state = State.PARAMS
                 elif 'SQL.Strings' in line:
                     state = State.STRINGLIST
+            elif state == State.LABELEDIT:
+                if line == 'end':
+                    state = State.OUTER
+                elif 'Pesquisa.Select.Strings' in line:
+                    state = State.STRINGLIST
+            elif state == State.PARAMS:
+                if line == 'end>':
+                    state = State.QUERY
             elif state == State.STRINGLIST:
                 if line.endswith(')'):
                     state = State.QUERY
                 elif line.endswith('+'):
                     state = State.LINEMERGE
-                queries[query_name].append(clear_line(line))
+                queries[component_name].append(clear_line(line))
             elif state == State.LINEMERGE:
                 if line.endswith(')'):
                     state = State.QUERY
                 elif not line.endswith('+'):
                     state = State.STRINGLIST
-                queries[query_name][-1] += clear_line(line)
+                queries[component_name][-1] += clear_line(line)
         return queries
 
 
@@ -70,18 +87,30 @@ def search_for(search_term, filename):
 
 
 def search_in_dfm(search_term, filename):
-    result = search_for(search_term, filename)
-    if result:
-        print('Found in "{}". Queries: {}'.format(filename, ', '.join(result)))
+    components = search_for(search_term, filename)
+    if components:
+        print('Found in "{}":'.format(filename))
+        for component in components:
+            print('  {}'.format(component))
+
+
+def search_in_all_dfms(search_term):
+    for filename in glob.glob("**/*.dfm", recursive=True):
+        search_in_dfm(search_term, filename)
 
 
 def main():
     if len(sys.argv) < 2:
-        print('Uso: dfmgrep [termo_pesquisado]')
+        print('Uso: dfmgrep [arquivo_dfm] <termo_pesquisado>')
         sys.exit(1)
-    search_term = sys.argv[1]
-    for filename in glob.glob("**/*.dfm", recursive=True):
-        search_in_dfm(search_term, filename)
+
+    if len(sys.argv) == 2:
+        search_term = sys.argv[1]
+        search_in_all_dfms(search_term)
+    elif len(sys.argv) == 3:
+        dfm_file = sys.argv[1]
+        search_term = sys.argv[2]
+        search_in_dfm(search_term, dfm_file)
 
 
 if __name__ == "__main__":
