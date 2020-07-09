0eb84aa6a615d962a90040983aa93780322ae870
Clayton <clayton.aa@gmail.com>
2020-07-09 17:27:48 -0400

    Corrige processamento de mensagens de commit para tratar nomes de autores com espaços

--- ello/sdk/text_manipulation.py
+++ ello/sdk/text_manipulation.py
@@ -31,7 +31,7 @@ def crop_message(text):
 
 def remove_marked_messages(text):
     """ Ignorar mensagens de commit que contenham '*' no final """
-    return not re.search(r'\* <\w+>$', text)
+    return not re.search(r'\* <[\w ]+>$', text)
 
 
 def remove_issue_number(text):
