d58ccee1e19177ebf8094ba491dbcb864093e9df
Clayton <clayton.aa@gmail.com>
2021-04-22 16:07:20 -0400

    Adiciona valores padrão caso o arquivo de configuração não exista

--- ello/sdk/config.py
+++ ello/sdk/config.py
@@ -1,24 +1,21 @@
-#coding: utf8
 import os
+import os.path
 from configparser import ConfigParser
 
-user_folder = os.path.expanduser("~")
-config_filename = "{}\\ello-builder.ini".format(user_folder)
+config_filename = os.path.join(os.path.expanduser('~'), 'ello-sdk.ini')
 
 config = ConfigParser()
 config.read(config_filename)
 
-hostname = config.get('servidor', 'hostname')
-ssh_port = config.get('servidor', 'ssh_port')
-ssh_user = config.get('servidor', 'ssh_user')
-ssh_key  = config.get('servidor', 'ssh_key')
+hostname = config.get('servidor', 'hostname', fallback='server')
+ssh_port = config.get('servidor', 'ssh_port', fallback='2202')
+ssh_user = config.get('servidor', 'ssh_user', fallback='user')
+ssh_key  = config.get('servidor', 'ssh_key', fallback='id_rsa')
 
-ftp_path = config.get('ftp', 'path')
+wiki_user = config.get('wiki', 'user', fallback='')
+wiki_password = config.get('wiki', 'password', fallback='')
 
-wiki_user = config.get('wiki', 'user')
-wiki_password = config.get('wiki', 'password')
-
-whatsapp_url     = config.get('whatsapp', 'url')
-whatsapp_numbers = [n.strip() for n in config.get('whatsapp', 'notify_numbers').split(',')]
-telegram_token = config.get('telegram', 'token')
+whatsapp_url     = config.get('whatsapp', 'url', fallback='')
+whatsapp_numbers = [n.strip() for n in config.get('whatsapp', 'notify_numbers', fallback='').split(',')]
+telegram_token = config.get('telegram', 'token', fallback='')
 
