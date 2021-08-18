401edc7d4b37a3e28cfc8b7ddabfc449f030eb12
Clayton <clayton.aa@gmail.com>
2021-08-18 15:07:43 -0400

    Corrige crgrep para pesquisar também em subreports do Crystal Reports

--- ello/cli/crgrep.py
+++ ello/cli/crgrep.py
@@ -7,15 +7,35 @@ import glob
 import pywintypes
 from win32com.client import Dispatch
 
+SUBREPORT_KIND = 5
+
+
 def search_in_report(search_term, report_file):
     app = Dispatch('CrystalRunTime.Application')
-    rep = app.OpenReport(report_file)
-    rep.enableParameterPrompting = False
+    report = app.OpenReport(report_file)
+    
+    if _search_in_report(search_term, report):
+        print(f'Found in "{report_file}"')
+
+    for section in report.Sections:
+        for report_object in section.ReportObjects:
+            if report_object.Kind == SUBREPORT_KIND:
+                subreport = report_object.OpenSubreport()
+                if _search_in_report(search_term, subreport):
+                    print(f'Found in "{report_file}" section ' + report_object.Name)
+
+
+def _search_in_report(search_term, report):
+    report.enableParameterPrompting = False
+    
     try:
-        if search_term.lower() in rep.sqlquerystring.lower():
-            print(f'Found in "{report_file}"')
+        if search_term.lower() in report.sqlquerystring.lower():
+            return True
     except pywintypes.com_error as e:
-        print(f'Error in "{report_file}":\n\t{e.args}')
+        #print(f'Error in "{report_file}":\n\t{e.args}')
+        print(f'Error:\n\t{e.args}')
+
+    return False
 
 
 def search_in_all_reports(search_term):
